c573ab83e5e7689174c38a7fc488e596af12dac6
kvrakshith404-cmd <rakshith@kvgce.ac.in>
2026-09-15 21:30:56 +0530

    Linear Search

---
+++ Array/LInear_search.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class LInear_search {
+    public static int Linear(int a[],int tar){
+        for(int i=0;i<a.length;i++){
+            if(a[i]==tar){
+                return i;
+            }
+        }
+        // if not exist
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        int a[]={ 4,1,8,9,2,5};
+        System.out.println("Enter the search number:");
+        int target=input.nextInt();
+        int index=Linear(a, target);
+        if(index==-1){
+            System.out.println("Element not present.");
+        }
+        else{
+            System.out.println("Element Prsent in array at index:"+index);
+        }
+
+    }
+
+    
+}
